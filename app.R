4634bcb9c49f9cd88c15c9e83a83f90f00733d30
Lina Benson <lina.benson@ki.se>
2025-10-30 11:27:33 +0100

    changed to bslib

--- app.R
+++ app.R
@@ -1,32 +1,23 @@
-library(shiny)
-library(shinythemes)
-library(bslib)
-library(shinyWidgets)
-library(dplyr)
-
 source("setup.R")
 
-# Define UI for random distribution app ----
-ui <- fluidPage(
-  # theme = bs_theme(preset = "bootstrap"),
-  theme = shinytheme("spacelab"),
-  # App title ----
-  titlePanel("Eligibility and event rates - SwedeHF"),
-
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-      h4(strong("Select patients:")),
+ui <- page_sidebar(
+  title = h4("Eligibility and event rates - SwedeHF"),
+  theme = mytheme,
+  sidebar = sidebar(
+    width = "30%",
+    fillable = FALSE,
+    card(
+      card_header(h5("Ejection fraction (%)"), class = "bg-primary"),
       selectInput(
         inputId = "ef",
-        label = "Ejection Fraction (%)",
+        label = NULL,
         choices = appvar_values$shf_ef,
         selected = appvar_values$shf_ef,
         multiple = TRUE
-      ),
-      br(),
-      h4(strong("Eligibility criteria:")),
+      )
+    ),
+    card(
+      card_header(h5("Eligibility criteria"), class = "bg-primary"),
       selectInput(
         inputId = "prevhfh6mo",
         label = "HF hospitalization < 6 months",
@@ -43,7 +34,7 @@ ui <- fluidPage(
       ),
       selectInput(
         inputId = "af",
-        label = "Atrial Fibrillation",
+        label = "Atrial fibrillation",
         choices = appvar_values$shf_sos_com_af,
         selected = appvar_values$shf_sos_com_af,
         multiple = TRUE
@@ -69,52 +60,61 @@ ui <- fluidPage(
         selected = appvar_values$shf_bpsys,
         multiple = TRUE
       )
-    ),
-    # Main panel for displaying outputs ----
-    mainPanel(
-      # Output: Tabset w/ plot, summary, and table ----
-      tabsetPanel(
-        type = "tabs",
-        tabPanel(
-          title = "Eligibility and rates",
-          page_fillable(
-            # br(),
-            h4(uiOutput("error")),
-            br(),
-            layout_column_wrap(
-              # width = "250px",
-              fill = FALSE,
-              value_box(
-                title = NULL,
-                uiOutput("eligibilty"),
-                showcase = bsicons::bs_icon("people-fill"),
-                theme = "bg-success"
-              )
-            ),
-            # card(
-            DT::dataTableOutput(outputId = "base")
-            # )
-            ,
-            br(),
-            # card(
-            DT::dataTableOutput(outputId = "event")
-            # )
-          )
-        ),
-        tabPanel(
-          "Information",
-          value = "Information",
-          icon = icon("info"),
-          br(),
-          p("The displayed information is the last registration / patient in the Swedish Heart Failure Registry (SwedeHF) 2016-2023 with NYHA class II, III or IV, non-missing Ejection fraction and discharged alive from hospital (N = 46,636)."),
-          p("The outcomes were derived from the National Patient register (hospitalizations) and The Cause of Death Register (deaths). HF was defined as ICD-10 I110, I130, I132, I255, I420, I423, I425-9, I43, I50, J81, K761, R570 and CV as I, J81, K761, R570, G45."),
-          p("The displayed information is the last registration / patient in the Swedish Heart Failure Registry (SwedeHF) 2016-2023 with NYHA class II, III or IV and discharged alive from hospital."),
-          p("Missing values are imputed with a multivariate imputation algorithm based on random forests (Mayer M (2024). _missRanger: Fast Imputation of Missing Values_. doi:10.32614/CRAN.package.missRanger")
-        )
+    ), card(
+      card_header(h5("Event type"), class = "bg-primary"),
+      selectInput(
+        inputId = "out",
+        label = NULL,
+        choices = outvars$name,
+        selected = outvars$name,
+        multiple = TRUE
       )
+    )
+  ),
+  navset_card_underline(
+    nav_panel(
+      title = "Table",
+      icon = icon("table"),
+      #             page_fillable(
+      # br(),
+      uiOutput("error"),
+      # width = "250px",
+      # fill = FALSE,
+      value_box(
+        title = NULL,
+        uiOutput("eligibilty"),
+        showcase = bsicons::bs_icon("people-fill"),
+        theme = "bg-primary"
+      ),
+      # card(
+      DT::dataTableOutput(outputId = "base"),
+      padding = 0
+      # )
+      ,
+      # card(
+      DT::dataTableOutput(outputId = "event")
+      # )
+      # )
+    ),
+    nav_panel(
+      title = "Graph",
+      icon = icon("chart-simple"),
+      page_fillable(
+        uiOutput("error2"),
+        plotOutput("plot")
+      )
+    ),
+    nav_panel("Information",
+      value = "Information",
+      icon = icon("info"),
+      p("The displayed information is the last registration / patient in the Swedish Heart Failure Registry (SwedeHF) 2016-2023 with recorded Ejection fraction and discharged alive from hospital (N = 46,636)."),
+      p("The outcomes were derived from the National Patient register (hospitalizations) and The Cause of Death Register (deaths). HF was defined as ICD-10 I110, I130, I132, I255, I420, I423, I425-9, I43, I50, J81, K761, R570 and CV as I, J81, K761, R570, G45."),
+      p("The displayed information is the last registration / patient in the Swedish Heart Failure Registry (SwedeHF) 2016-2023 with NYHA class II, III or IV and discharged alive from hospital."),
+      p("Missing values are imputed with a multivariate imputation algorithm based on random forests (Mayer M (2024). _missRanger: Fast Imputation of Missing Values_. doi:10.32614/CRAN.package.missRanger")
     )
   )
 )
+
 
 server <- function(input, output) {
   select_func2 <- reactive({
@@ -133,7 +133,7 @@ server <- function(input, output) {
     if (select_func2() == 0) {
       checkempty <- c(is.null(input$ef), is.null(input$prevhfh6mo), is.null(input$nyha), is.null(input$af), is.null(input$gfrckdepi), is.null(input$ntprobnp), is.null(input$bpsys))
       labs <- c("Ejection Fraction", "HF hospitalization", "NYHA class", "Atrial Fibrillation", "eGFR", "NT-proBNP", "Systolic blood pressure")
-      paste0("<br /><font color=\"#FF0000\"><b>Please select at least one value for ", paste0(labs[checkempty], collapse = " and "), "</b></font>")
+      paste0("<font color=\"#FF0000\"><b>Select at least one value for ", paste0(labs[checkempty], collapse = " and "), "</b></font>")
     } else if (select_func2() == -1) {
       checkad <- c(
         "Ejection Fraction"[any(diff(sort(as.numeric(input$ef))) > 1)],
@@ -144,7 +144,28 @@ server <- function(input, output) {
         "NT-proBNP"[any(diff(sort(as.numeric(input$ntprobnp))) > 1)],
         "Systolic blood pressure"[any(diff(sort(as.numeric(input$bpsys))) > 1)]
       )
-      paste0("<br /><font color=\"#FF0000\"><b>All selected categories need to be numerically ajoining for ", paste0(checkad, collapse = " and "), "</b></font>")
+      paste0("<font color=\"#FF0000\"><b>All selected categories must be numerically ajoining for ", paste0(checkad, collapse = " and "), "</b></font>")
+    } else if (!select_func2() %in% c(0, -1)) {
+      NULL
+    }
+  })
+
+  output$error2 <- renderText({
+    if (select_func2() == 0 | is.null(input$out)) {
+      checkempty <- c(is.null(input$ef), is.null(input$prevhfh6mo), is.null(input$nyha), is.null(input$af), is.null(input$gfrckdepi), is.null(input$ntprobnp), is.null(input$bpsys), is.null(input$out))
+      labs <- c("Ejection Fraction", "HF hospitalization", "NYHA class", "Atrial Fibrillation", "eGFR", "NT-proBNP", "Systolic blood pressure", "Outcome")
+      paste0("<font color=\"#FF0000\"><b>Select at least one value for ", paste0(labs[checkempty], collapse = " and "), "</b></font>")
+    } else if (select_func2() == -1) {
+      checkad <- c(
+        "Ejection Fraction"[any(diff(sort(as.numeric(input$ef))) > 1)],
+        "HF hospitalization"[any(diff(sort(as.numeric(input$prevhfh6mo))) > 1)],
+        "NYHA class"[any(diff(sort(as.numeric(input$nyha))) > 1)],
+        "Atrial Fibrillation"[any(diff(sort(as.numeric(input$af))) > 1)],
+        "eGFR"[any(diff(sort(as.numeric(input$gfrckdepi))) > 1)],
+        "NT-proBNP"[any(diff(sort(as.numeric(input$ntprobnp))) > 1)],
+        "Systolic blood pressure"[any(diff(sort(as.numeric(input$bpsys))) > 1)]
+      )
+      paste0("<font color=\"#FF0000\"><b>All selected categories must be numerically ajoining for ", paste0(checkad, collapse = " and "), "</b></font>")
     } else if (!select_func2() %in% c(0, -1)) {
       NULL
     }
@@ -154,7 +175,7 @@ server <- function(input, output) {
     if (select_func2() %in% c(-1, 0)) {
       return(NULL)
     } else {
-      paste0(el[[select_func2()]], "<br> of patients with EF ", paste0(names(appvar_values$shf_ef)[appvar_values$shf_ef %in% input$ef], collapse = ", "), "% are eligible")
+      paste0(el[[select_func2()]], "% eligibility for patients with EF ", paste0(names(appvar_values$shf_ef)[appvar_values$shf_ef %in% input$ef], collapse = ", "), "%")
     }
   })
 
@@ -190,7 +211,9 @@ server <- function(input, output) {
       return(NULL)
     } else {
       DT::datatable(
-        rate[[select_func2()]],
+        rate[[select_func2()]] %>%
+          filter(outname %in% c(input$out)) %>%
+          select(-rate_0, -rate_1),
         extensions = "Buttons",
         options = list(
           paging = FALSE,
@@ -212,6 +235,13 @@ server <- function(input, output) {
         colnames = c("", "Time frame", "Not eligible, events/100 person-years (95% CI)", "Eligible, events/100 person-years (95% CI)"),
         escape = F
       )
+    }
+  )
+  output$plot <- renderPlot(
+    if (select_func2() %in% c(-1, 0) | is.null(input$out)) {
+      return(NULL)
+    } else {
+      figfunc(rate[[select_func2()]] %>% filter(outname %in% c(input$out)))
     }
   )
 }
--- setup.R
+++ setup.R
@@ -1,3 +1,12 @@
+library(shiny)
+library(shinythemes)
+library(bslib)
+library(shinyWidgets)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(stringr)
+
 select_func <- function(ef, prevhfh6mo, nyha,
                         af, gfrckdepi, ntprobnp, bpsys) {
   checkempty <- c(is.null(ef), is.null(prevhfh6mo), is.null(nyha), is.null(af), is.null(gfrckdepi), is.null(ntprobnp), is.null(bpsys))
@@ -22,5 +31,44 @@ select_func <- function(ef, prevhfh6mo, nyha,
 
   return(allcombsout)
 }
+
+figfunc <- function(subdata) {
+  subdata <- subdata %>%
+    select(-r_0, -r_1) %>%
+    pivot_longer(cols = c(rate_0, rate_1), names_to = "el", values_to = "rate") %>%
+    filter(!is.na(rate)) %>%
+    mutate(el = factor(str_remove(el, "rate_"), levels = c("0", "1"), labels = c("Not eligible", "Eligible")))
+
+  minmax <- c(0, ceiling((max(subdata$rate) + max(subdata$rate) * .1) / 10) * 10)
+  ggplot(subdata, aes(x = time, y = rate, fill = el)) +
+    geom_bar(stat = "identity", position = "dodge") +
+    scale_y_continuous(limits = minmax, breaks = minmax) +
+    facet_wrap(vars(outname), ncol = 1) + # , scales = "free_y") +
+    scale_fill_manual(values = c("#00688B", "darkseagreen4")) +
+    theme_classic() +
+    theme(
+      text = element_text(face = "bold", size = 16),
+      legend.position = "bottom",
+      legend.box = "vertical",
+      axis.text = element_text(color = "black"),
+      axis.title.x = element_blank(),
+      legend.title = element_blank()
+    ) +
+    labs(y = "Events/100 person-years") +
+    geom_text(aes(label = round(rate, 0)), position = position_dodge(width = 0.9), vjust = -0.25, fontface = "bold")
+}
+
+mytheme <- bs_theme(
+  # Controls the default grayscale palette
+  # bg = "#DCDCDC",  fg = "#698B69",
+  # Controls the accent (e.g., hyperlink, button, etc) colors
+  primary = "#00688B", secondary = "#009ACD",
+  # base_font = c("Grandstander", "sans-serif"),
+  base_font = "'Helvetica Neue', Helvetica, sans-serif",
+  code_font = c("Courier", "monospace"),
+  heading_font = "'Helvetica Neue', Helvetica, sans-serif",
+  # Can also add lower-level customization
+  "input-border-color" = "#00688B"
+)
 
 load("./data/shinydata.RData")
